9eb77d205ebf1df29fcc1170b175ba434eb47484
TheRavshanDev <88030420+TheRavshanDev@users.noreply.github.com>
2022-06-18 16:06:47 +0500

    Added requirements.txt

--- base/forms.py
+++ base/forms.py
@@ -1,6 +1,4 @@
 from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 from captcha.fields import CaptchaField
 
 class RegisterUserForm(forms.Form):
